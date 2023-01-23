fd3950e043667ec327e0eb43d0584a71d03873b6
Fábio Leandro <fabio.leandro@evolutap.com.br>
2023-01-23 10:20:50 -0300

    Remocao dos metodos find e update

--- Crud.php
+++ Crud.php
@@ -7,15 +7,15 @@ abstract class Crud extends DB {
 	protected $table;
 
 	abstract public function create();
-	abstract public function update( $id );
+	// abstract public function update( $id );
 
-	public function find( $id ) {
-		$sql  = "SELECT * FROM $this->table WHERE id = :id";
-		$stmt = DB::prepare( $sql );
-		$stmt->bindParam( ':id', $id, PDO::PARAM_INT );
-		$stmt->execute();
-		return $stmt->fetchAll();
-	}
+	// public function find( $id ) {
+	// 	$sql  = "SELECT * FROM $this->table WHERE id = :id";
+	// 	$stmt = DB::prepare( $sql );
+	// 	$stmt->bindParam( ':id', $id, PDO::PARAM_INT );
+	// 	$stmt->execute();
+	// 	return $stmt->fetchAll();
+	// }
 
 	public function findAll() {
 		$sql  = "SELECT * FROM $this->table";
