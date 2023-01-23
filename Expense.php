ae84029583ace143de0679e638a2b0fd3fc3b4f4
Fábio Leandro <fabio.leandro@evolutap.com.br>
2023-01-23 00:46:47 -0300

    Implementado metodo create_api

--- Expense.php
+++ Expense.php
@@ -3,15 +3,73 @@ require_once 'Crud.php';
 
 class Expense extends Crud {
 	protected $table = 'despesas';
-	public $data;
+	private $data;
+	public $valor;
+	public $data_compra;
+	public $descricao;
+	public $tipo_pagamento_id;
+	public $categoria_id;
 
-	function __construct( $data ) {
-		$this->data = $data;
-	}	
-	public function update($id) { return true; }
+	public function __construct( $valor, $data_compra, $descricao, $tipo_pagamento_id, $categoria_id ) {
+		$this->valor = $valor;
+		$this->data_compra = $data_compra;
+		$this->descricao = $descricao;
+		$this->tipo_pagamento_id = $tipo_pagamento_id;
+		$this->categoria_id = $categoria_id;
+	}
 
-	public function create() {
-		foreach( $this->data as $expense ) :
+	// public function getData(){
+	// 	return $this->data;
+	// }
+
+	// public function getValor(){
+	// 	return $this->valor;
+	// }
+
+	// public function getDataCompra(){
+	// 	return $this->data_compra;
+	// }
+
+	// public function getDescricao(){
+	// 	return $this->descricao;
+	// }
+	
+	// public function getTipoPagamentoId(){
+	// 	return $this->tipo_pagamento_id;
+	// }
+
+	// public function getCategoriaId(){
+	// 	return $this->categoria_id;
+	// }
+
+	// public function setData($data){
+	// 	$this->data = $data;
+	// }
+
+	// public function setValor($valor){
+	// 	$this->valor = $valor;
+	// }
+
+	// public function setDataCompra($data_compra){
+	// 	$this->data_compra = $data_compra;
+	// }
+
+	// public function setDescricao($descricao){
+	// 	$this->descricao = $descricao;
+	// }
+
+	// public function setTipoPagamentoId($tipo_pagamento_id){
+	// 	$this->tipo_pagamento_id = $tipo_pagamento_id;
+	// }
+
+	// public function setCategoriaId($categoria_id){
+	// 	$this->categoria_id = $categoria_id;
+	// }
+
+	public function update( $id ) { return true; }
+
+	public function create_api() {
+		foreach( $this->getData() as $expense ) :
 			$sql  = "INSERT INTO $this->table (valor, data_compra, descricao, tipo_pagamento_id, categoria_id) VALUES (:valor, :data_compra, :descricao, :tipo_pagamento_id, :categoria_id)";
 			$stmt = DB::prepare( $sql );
 			$stmt->bindParam( ':valor', $expense->valor );
@@ -23,8 +81,19 @@ class Expense extends Crud {
 		endforeach;
 	}
 
+	public function create() {
+		$sql  = "INSERT INTO $this->table (valor, data_compra, descricao, tipo_pagamento_id, categoria_id) VALUES (:valor, :data_compra, :descricao, :tipo_pagamento_id, :categoria_id)";
+		$stmt = DB::prepare( $sql );
+		$stmt->bindParam( ':valor', $this->valor );
+		$stmt->bindParam( ':data_compra', $this->data_compra );
+		$stmt->bindParam( ':descricao', $this->descricao );
+		$stmt->bindParam( ':tipo_pagamento_id', $this->tipo_pagamento_id );
+		$stmt->bindParam( ':categoria_id', $this->categoria_id );
+		return $stmt->execute(); 
+	}
+
 	public function store() {
-		$sql = "SELECT * FROM despesas AS despesa JOIN tipo_pagamento AS tipo WHERE despesa.tipo_pagamento_id = tipo.id";
+		$sql = "SELECT * FROM despesas AS despesa JOIN tipo_pagamento AS tipo JOIN categorias AS categoria WHERE despesa.tipo_pagamento_id = tipo.id AND despesa.categoria_id = categoria.id";
 		$stmt = DB::prepare( $sql );
 		$stmt->execute();
 		return $stmt->fetchAll();
