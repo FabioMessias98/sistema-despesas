b1deea8ea48f85efa22e9f909727bba26f77bad2
Fábio Leandro <fabio.leandro@evolutap.com.br>
2023-01-23 20:37:23 -0300

    Implementado a tela de deletar de despesas

---
+++ expenses/delete.php
@@ -0,0 +1,10 @@
+<?php
+    include( '../Expense.php' );
+
+    if( $_GET['id'] ) {
+        $expenses = new Expense();
+        $expenses->delete( $_GET['id'] );
+    }
+
+    header( 'Location: ../index.php' );
+?>
