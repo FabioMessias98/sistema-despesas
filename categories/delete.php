02caab09f9ed2f651ab268d4f47ac6dc1838df7b
Fábio Leandro <fabio.leandro@evolutap.com.br>
2023-01-23 20:35:16 -0300

    Implementado a tela de deletar categoria

---
+++ categories/delete.php
@@ -0,0 +1,10 @@
+<?php
+    include( '../Category.php' );
+
+    if( $_GET['id'] ) {
+        $categories = new Category();
+        $categories->delete( $_GET['id'] );
+    }
+
+    header( 'Location: ../index.php' );
+?>
