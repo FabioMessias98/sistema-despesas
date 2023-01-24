6760dead5c890a281994eae48c4e8d612d9221ad
Fábio Leandro <fabio.leandro@evolutap.com.br>
2023-01-24 18:23:38 -0300

    Modificado estrutura da tabela para gerar excel

--- report/excel/index.php
+++ report/excel/index.php
@@ -68,6 +68,8 @@
             
             $html .= "</table>";
 
+            echo $html;
+
             header ( "Expires: Mon, 26 Jul 1997 05:00:00 GMT" );
             header ( "Last-Modified: " . gmdate("D,d M YH:i:s" ) . " GMT" );
             header ( "Cache-Control: no-cache, must-revalidate" );
@@ -75,7 +77,6 @@
             header ( "Content-type: application/x-msexcel" );
             header ( "Content-Disposition: attachment; filename=\"{$file}\"" );
             header ( "Content-Description: PHP Generated Data" );
-            // header( "Location: /sistema-despesas/pages/expense/");
 		    exit;
         ?>
     </body>
