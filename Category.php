378bec82976357823494927fa4bf0e6fab962398
Fábio Leandro <fabio.leandro@evolutap.com.br>
2023-01-24 18:16:59 -0300

    Removido codigo comentado na classe Category

--- Category.php
+++ Category.php
@@ -54,25 +54,7 @@ class Category extends Crud {
 		$stmt->bindParam(':descricao', $this->descricao);
 		$stmt->bindParam(':id', $id);
 		return $stmt->execute();	
-	}
-
-	// public function recover(){
-	//     $sql  = "Select * from $this->table where email =:email AND senha = :senha";
-	// 	$stmt = DB::prepare($sql);
-	// 	$stmt->bindParam(':email', $this->email);
-	// 	$stmt->bindParam(':senha', $this->senha);
-	// 	$stmt->execute(); 
-	// 	return $stmt->fetchAll();
-	// }
-	
-	// public function resetPassword(){
-	//     $sql  = "UPDATE $this->table SET  senha = :senha WHERE id = :id";
-	//     $stmt = DB::prepare($sql);
-	//     $stmt->bindParam(':senha', $this->senha);
-	//     $stmt->bindParam(':id', $this->id);
-	//     return $stmt->execute();
-	// }
-	
+	}	
 }
 
 ?>
